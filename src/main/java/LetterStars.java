d09840d4247a1779f92abbd52bf97ec3779b2270
Polya404 <polyapolya404@gmail.com>
2022-07-05 13:16:04 +0300

    Initial commit

---
+++ src/main/java/LetterStars.java
@@ -0,0 +1,33 @@
+
+public class LetterStars {
+
+    private static void fillArrFromOther(char[] distArr, int beginIndexDis, int endIndexDis, char[] source) {
+        int sourcInd = 0;
+        for (int i = beginIndexDis; i < endIndexDis; i++) {
+            distArr[i] = source[sourcInd++];
+        }
+    }
+
+    static void printPhraz(char[][][] arr, int len) {
+        int count = 0;
+        char[][] pole = new char[arr[0].length][arr.length * arr[0][0].length + arr[0].length];
+        ;
+        for (int j = 0; j < arr.length; j++) {
+            for (int i = 0; i < arr[0].length; i++) {
+                for (int k = 0; k < len; k++) {
+                    fillArrFromOther(pole[i], k * arr[0][i].length + k, (k + 1) * arr[0][i].length + k, arr[k][i]);
+                }
+            }
+        }
+        for (char[] row : pole) {
+            for (char elem : row) {
+                if (elem == ' ' | elem == '\u0000')
+                    System.out.printf("%2c", ' ');
+                else
+                    System.out.printf("%2c", '@');
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ src/main/java/NumberPrint.java
@@ -0,0 +1,106 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class NumberPrint {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        HashMap<String, char[][]> nums = new HashMap<>();
+        if (args.length == 0) {
+            System.out.println("Enter numbers");
+        }
+        for (String arg : args) {
+            switch (arg) {
+                case "0":
+                    arrayList.add("0");
+                    nums.put(arg, Numbers.zero);
+                    break;
+                case "1":
+                    arrayList.add("1");
+                    nums.put(arg, Numbers.one);
+                    break;
+                case "2":
+                    arrayList.add("2");
+                    nums.put(arg, Numbers.two);
+                    break;
+                case "3":
+                    arrayList.add("3");
+                    nums.put(arg, Numbers.three);
+                    break;
+                case "4":
+                    arrayList.add("4");
+                    nums.put(arg, Numbers.four);
+                    break;
+                case "5":
+                    arrayList.add("5");
+                    nums.put(arg, Numbers.five);
+                    break;
+                case "6":
+                    arrayList.add("6");
+                    nums.put(arg, Numbers.six);
+                    break;
+                case "7":
+                    arrayList.add("7");
+                    nums.put(arg, Numbers.seven);
+                    break;
+                case "8":
+                    arrayList.add("8");
+                    nums.put(arg, Numbers.eight);
+                    break;
+                case "9":
+                    arrayList.add("9");
+                    nums.put(arg, Numbers.nine);
+                    break;
+                default:
+                    System.out.println("Enter only numbers");
+            }
+
+        }
+        char[][][] res = new char[args.length][][];
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            switch (arrayList.get(i)) {
+                case "0":
+                    res[i] = nums.get("0");
+                    break;
+                case "1":
+                    res[i] = nums.get("1");
+                    break;
+                case "2":
+                    res[i] = nums.get("2");
+                    break;
+                case "3":
+                    res[i] = nums.get("3");
+                    break;
+                case "4":
+                    res[i] = nums.get("4");
+                    break;
+                case "5":
+                    res[i] = nums.get("5");
+                    break;
+                case "6":
+                    res[i] = nums.get("6");
+                    break;
+                case "7":
+                    res[i] = nums.get("7");
+                    break;
+                case "8":
+                    res[i] = nums.get("8");
+                    break;
+                case "9":
+                    res[i] = nums.get("9");
+                    break;
+                default:
+                    System.out.println("Enter only numbers");
+            }
+        }
+        try {
+            LetterStars.printPhraz(res, args.length);
+        }catch (NullPointerException e){
+            e.getMessage();
+        }
+
+
+    }
+
+}
