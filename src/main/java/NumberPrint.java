756b8f7002ee9b71ae7a842081e7997baac0250d
Polya404 <polyapolya404@gmail.com>
2022-07-08 14:51:40 +0300

    Commit

--- src/main/java/NumberPrint.java
+++ src/main/java/NumberPrint.java
@@ -5,98 +5,100 @@ import java.util.HashMap;
 public class NumberPrint {
     public static void main(String[] args) {
         ArrayList<String> arrayList = new ArrayList<>();
-        HashMap<String, char[][]> nums = new HashMap<>();
+        HashMap<Character, char[][]> nums = new HashMap<>();
+
         if (args.length == 0) {
             System.out.println("Enter numbers");
         }
-        for (String arg : args) {
-            switch (arg) {
-                case "0":
+        char[] str = args[0].toCharArray();
+        for (Character string : str) {
+            switch (string) {
+                case '0':
                     arrayList.add("0");
-                    nums.put(arg, Numbers.zero);
+                    nums.put(string, Numbers.zero);
                     break;
-                case "1":
+                case '1':
                     arrayList.add("1");
-                    nums.put(arg, Numbers.one);
+                    nums.put(string, Numbers.one);
                     break;
-                case "2":
+                case '2':
                     arrayList.add("2");
-                    nums.put(arg, Numbers.two);
+                    nums.put(string, Numbers.two);
                     break;
-                case "3":
+                case '3':
                     arrayList.add("3");
-                    nums.put(arg, Numbers.three);
+                    nums.put(string, Numbers.three);
                     break;
-                case "4":
+                case '4':
                     arrayList.add("4");
-                    nums.put(arg, Numbers.four);
+                    nums.put(string, Numbers.four);
                     break;
-                case "5":
+                case '5':
                     arrayList.add("5");
-                    nums.put(arg, Numbers.five);
+                    nums.put(string, Numbers.five);
                     break;
-                case "6":
+                case '6':
                     arrayList.add("6");
-                    nums.put(arg, Numbers.six);
+                    nums.put(string, Numbers.six);
                     break;
-                case "7":
+                case '7':
                     arrayList.add("7");
-                    nums.put(arg, Numbers.seven);
+                    nums.put(string, Numbers.seven);
                     break;
-                case "8":
+                case '8':
                     arrayList.add("8");
-                    nums.put(arg, Numbers.eight);
+                    nums.put(string, Numbers.eight);
                     break;
-                case "9":
+                case '9':
                     arrayList.add("9");
-                    nums.put(arg, Numbers.nine);
+                    nums.put(string, Numbers.nine);
                     break;
                 default:
                     System.out.println("Enter only numbers");
             }
 
         }
-        char[][][] res = new char[args.length][][];
+        char[][][] res = new char[str.length][][];
 
         for (int i = 0; i < arrayList.size(); i++) {
             switch (arrayList.get(i)) {
                 case "0":
-                    res[i] = nums.get("0");
+                    res[i] = nums.get('0');
                     break;
                 case "1":
-                    res[i] = nums.get("1");
+                    res[i] = nums.get('1');
                     break;
                 case "2":
-                    res[i] = nums.get("2");
+                    res[i] = nums.get('2');
                     break;
                 case "3":
-                    res[i] = nums.get("3");
+                    res[i] = nums.get('3');
                     break;
                 case "4":
-                    res[i] = nums.get("4");
+                    res[i] = nums.get('4');
                     break;
                 case "5":
-                    res[i] = nums.get("5");
+                    res[i] = nums.get('5');
                     break;
                 case "6":
-                    res[i] = nums.get("6");
+                    res[i] = nums.get('6');
                     break;
                 case "7":
-                    res[i] = nums.get("7");
+                    res[i] = nums.get('7');
                     break;
                 case "8":
-                    res[i] = nums.get("8");
+                    res[i] = nums.get('8');
                     break;
                 case "9":
-                    res[i] = nums.get("9");
+                    res[i] = nums.get('9');
                     break;
                 default:
-                    System.out.println("Enter only numbers separated by space");
+                    System.out.println("Enter only numbers");
             }
         }
         try {
-            LetterStars.printPhraz(res, args.length);
-        }catch (NullPointerException e){
+            LetterStars.printPhraz(res, str.length);
+        } catch (NullPointerException e) {
             e.getMessage();
         }
 
